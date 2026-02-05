bdfbdb6cc25b6e13c211cfaa2ff3e88a6193b8a2
chedli01 <chedli.masmoudi01@gmail.com>
2026-02-05 20:46:48 +0100

    connect to redis

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import app from './app';
 import { connectDatabase } from './config/databse';
+import { connectRedis } from './config/redis';
 
 const startServer = async (): Promise<void> => {
   try {
     await connectDatabase();
+    await connectRedis();
 
     app.listen(3000, () => {
       console.log(`Server running on port 3000`);
