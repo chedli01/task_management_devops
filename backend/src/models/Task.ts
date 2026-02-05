4dccd64281a499fa09c13793ca3b40dd69b86e2c
chedli01 <chedli.masmoudi01@gmail.com>
2026-02-05 19:15:41 +0100

    create User + Task models

---
+++ backend/src/models/Task.ts
@@ -0,0 +1,67 @@
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  ForeignKey,
+  BelongsTo,
+  PrimaryKey,
+  Default,
+} from 'sequelize-typescript';
+import User from './User';
+import { TaskStatus } from '@/enums/task-status.enum';
+import { TaskPriority } from '@/enums/task-priority.enum';
+
+@Table({
+  tableName: 'tasks',
+  timestamps: true,
+})
+export default class Task extends Model {
+  @PrimaryKey
+  @Default(DataType.UUIDV4)
+  @Column(DataType.UUID)
+  id!: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    validate: {
+      len: [1, 200],
+    },
+  })
+  title!: string;
+
+  @Column({
+    type: DataType.TEXT,
+    allowNull: true,
+  })
+  description?: string;
+
+  @Column({
+    type: DataType.ENUM(...Object.values(TaskStatus)),
+    defaultValue: TaskStatus.PENDING,
+  })
+  status!: TaskStatus;
+
+  @Column({
+    type: DataType.ENUM(...Object.values(TaskPriority)),
+    defaultValue: TaskPriority.MEDIUM,
+  })
+  priority!: TaskPriority;
+
+  @Column({
+    type: DataType.DATE,
+    allowNull: true,
+  })
+  dueDate?: Date;
+
+  @ForeignKey(() => User)
+  @Column({
+    type: DataType.UUID,
+    allowNull: false,
+  })
+  userId!: string;
+
+  @BelongsTo(() => User)
+  user!: User;
+}
