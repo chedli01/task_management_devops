e2bf14697344c1add1aa905e0b07a2b6682e3cfb
chedli01 <chedli.masmoudi01@gmail.com>
2026-02-11 15:31:48 +0100

    fix cd.yml

--- app-code/prod/frontend/src/components/tasks/TaskList.tsx
+++ app-code/prod/frontend/src/components/tasks/TaskList.tsx
@@ -44,7 +44,7 @@ const TaskList: React.FC = () => {
 
   return (
     <div className="space-y-6">
-      <h2 className="text-3xl font-bold">My Tasks - This is Chedli</h2>
+      <h2 className="text-3xl font-bold">My Tasks - This is Chedli - Part 2 testing</h2>
 
       <TaskForm onTaskCreated={handleTaskCreated} />
 
